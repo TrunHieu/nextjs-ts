1e0862894429fa7e74a57291725af9643fdf0e66
hieuht-flinters <hieu_ht@flintes.vn>
2022-06-04 23:39:57 +0700

    bookmark structure and edit user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,22 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import { JwtGuard } from '../auth/guard';
 import { GetUser } from '../auth/decorator';
 import { User } from '@prisma/client';
+import { UserService } from './user.service';
+import { EditUserDto } from './dto';
 
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
+  constructor(private userService: UserService) {}
+
   @Get('me')
   getMe(@GetUser() user: User) {
     return user;
+  }
+
+  @Patch()
+  editUser(@GetUser('id') userId: number, @Body() userDto: EditUserDto) {
+    return this.userService.editUser(userId, userDto);
   }
 }
